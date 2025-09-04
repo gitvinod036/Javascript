609345fada91fb27a8e0af044aabb94e683a43d8
gitvinod036 <sannapanenivinod036@gmail.com>
2025-09-04 23:12:11 +0530

    counter app

---
+++ script2.js
@@ -0,0 +1,25 @@
+function ONIncrement(){
+    let count_ele=document.getElementById("count")
+      let number=parseInt(count_ele.textContent)
+      count_ele.textContent=number+1
+
+      let val=parseInt(count_ele.textContent)
+      if (val>0){
+        count_ele.style.color="green"
+      }else{
+        count_ele.style.color="black"   
+      }
+}
+
+function ONDecrement(){
+  let count_ele=document.getElementById("count")
+  let number=parseInt(count_ele.textContent)
+  count_ele.textContent=number-1    
+
+  let val=parseInt(count_ele.textContent)
+       if (val<0){
+        count_ele.style.color="red"
+      }else{
+        count_ele.style.color="black"   
+      }
+}
